daac3cbc964e1adee885b07ca74a7487a7d99c93
Donald Blanc <dxb3662@rit.edu>
2018-05-27 13:01:29 -0400

    added heroku db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
 import mysql from  "mysql";
 //  const mysql = require("mysql")
 
+var connection;
 if(process.env.JAWSDB_URL){
-    const connection = mysql.createConnection(process.env.JAWSDB_URL)
+     connection = mysql.createConnection(process.env.JAWSDB_URL)
 
 }else{
 
-    const connection = mysql.createConnection({
+     connection = mysql.createConnection({
         host:"localhost",
         port: 3306,
         user: "root",
