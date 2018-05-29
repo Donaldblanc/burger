d0e438901e66881d06ebc0a585dd7b4b47c78dee
Donald Blanc <dxb3662@rit.edu>
2018-05-29 08:45:24 -0400

    added heroku db

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-// import mysql from  "mysql";
+ import mysql from  "mysql";
 // //  const mysql = require("mysql")
 
 // var connection;
@@ -31,33 +31,31 @@
 
 
 
-var mysql = require('mysql');
-
 // Create the MySQL connection object
-var connection = mysql.createConnection('mysql://h0vzo0sh80p5r4bq:be438oxgvml6fhbe@zf4nk2bcqjvif4in.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/pakf7aujdz7f84vl');
+var connection;
 
-// if (process.env.JAWSDB_URL) {
-// 	// DB is JawsDB on Heroku
-// 	connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-// 	// DB is local on localhost
-// 	connection = mysql.createConnection({
-// 		port: 3306,
-// 		host: 'localhost',
-// 		user: 'root',
-// 		password: 'root',
-// 		database: 'burgers_db'
-// 	})
-// };
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'root',
+		database: 'burgers_db'
+	})
+};
 
 
 // Make the connection to MySQL
 connection.connect(function(err) {
   if (err) {
-    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+    console.error(`ERROR: MySQL connection error --  ${err.stack }+ \n\n`);
     return;
   }
-  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+  console.log(`Connected to MySQL database as id  ${connection.threadId  } \n\n`);
 });
 
 // Export connection for ORM use
