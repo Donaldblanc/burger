c1e17d1aadf5a4e5c907cea9a01313e1d8a41b10
Donald Blanc <dxb3662@rit.edu>
2018-05-29 08:26:51 -0400

    added heroku db

--- server.js
+++ server.js
@@ -4,19 +4,27 @@ import express from 'express'
 import bodyParser from 'body-parser';
 import path from 'path';
 import routes from "./controllers/burgers_controller.js";
-import exphbs from 'express-handlebars'
+//import exphbs from 'express-handlebars'
 
 // const express = require ('express')
 // const bodyParser = require('body-parser')
 // const routes = require('./controllers/burgers_controller.js')
 // const exphbs = require('express-handlebars')
-
 let app = express();
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.set('views', path.join(__dirname,'/views'));  
+
+
 const PORT = process.env.PORT || 3000;
 
  app.use(express.static(__dirname + '/public'));
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
